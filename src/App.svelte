2bdbc51b9216a8ec2151f0afa8b2d261a2e7c718
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2023-07-07 15:20:52 +0700

    fix: randomized

--- src/App.svelte
+++ src/App.svelte
@@ -4,30 +4,43 @@
   let values = [
     {
       qoute:
-        '"searing memoir of reckoning and healing by acclaimed journalist Stephanie Foo, investigating the little-understood science behind compl"',
+        'searing maaaemoir of reckoning and healing by acclaimed journalist Stephanie Foo, investigating the little-understood science behind compl',
       book: "What My Bones Know: A Memoir of Healing from Complex Trauma",
       class: "deep-work",
       hashtags: "%23goodreads %23qoutes",
-      image:
-        "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1447957962i/25744928.jpg",
+    },
+      {
+      qoute:
+        'Blasg',
+      book: "What My Bones Know: A Memoir of Healing from Complex Trauma",
+      class: "deep-work",
+      hashtags: "%23goodreads %23qoutes",
     },
   ];
+
+
+  function getRandomEntry() {
+    const randomIndex = Math.floor(Math.random() * values.length);
+    return values[randomIndex];
+  }
+
+  $: randomEntry = getRandomEntry();
 </script>
 
 <main>
   <div
     id="content"
-    class="{values[0].class} h-4/6 w-3/4 shadow-md shadow-cyan-800/100 p-10"
+    class="{randomEntry.class} h-4/6 w-3/4 shadow-md shadow-cyan-800/100 p-10"
   >
-    <div>{values[0].qoute} | {values[0].book}</div>
+    <div>"{randomEntry.qoute}" <br/><br/><br/> {randomEntry.book}</div>
     <br />
     <div />
   </div>
   <div class="flex">
     <a
       class="twitter-share-button"
-      href="https://twitter.com/intent/tweet?text={values[0]
-        .qoute} %0A %0A {values[0].book} %0A %0A {values[0].hashtags}"
+      href="https://twitter.com/intent/tweet?text={randomEntry
+        .qoute} %0A %0A {randomEntry.book} %0A %0A {randomEntry.hashtags}"
       data-size="large"
     >
       <img src={twitterLogo} class="logo" alt="Svelte Logo" /></a
