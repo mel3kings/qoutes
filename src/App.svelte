39f332ed76fefb61648ea3623af104b6a7b226d8
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2023-07-14 12:28:20 +0700

    refactor: cleanup code

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,11 @@
 <script>
+  // @ts-nocheck
   import twitterLogo from "./assets/twitter.svg";
   import values from "./data.json";
+  import { onMount } from "svelte";
+  import auth from "./authService";
+  // @ts-ignore
+  import { isAuthenticated, user, user_tasks, tasks } from "./store";
 
   function getRandomEntry() {
     const randomIndex = Math.floor(Math.random() * values.length);
@@ -8,13 +13,6 @@
   }
 
   $: randomEntry = getRandomEntry();
-
-  import { onMount } from "svelte";
-  import auth from "./authService";
-  // @ts-ignore
-  import { isAuthenticated, user, user_tasks, tasks } from "./store";
-  // @ts-ignore
-  import TaskItem from "./components/TaskItem.svelte";
 
   let auth0Client;
   let newTask;
@@ -85,92 +83,19 @@
     >
       <img src={twitterLogo} class="logo" alt="Svelte Logo" /></a
     >
+    <div class="pt-10">
+    {#if !$isAuthenticated}
+      <a
+        class="btn btn-primary btn-lg mr-auto ml-auto"
+        href="/#"
+        role="button"
+        on:click={login}>Log In</a
+      >
+    {:else}
+      <a class="nav-link" href="/#" on:click={logout}>Log Out</a>
+    {/if}
+    </div>
   </div>
-  <!-- App Bar -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="/#">Task Manager</a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarText"
-      aria-controls="navbarText"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon" />
-    </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <div class="navbar-nav mr-auto user-details">
-        {#if $isAuthenticated}
-          <span class="text-white"
-            >&nbsp;&nbsp;{$user.name} ({$user.email})</span
-          >
-        {:else}<span>&nbsp;</span>{/if}
-      </div>
-      <span class="navbar-text">
-        <ul class="navbar-nav float-right">
-          {#if $isAuthenticated}
-            <li class="nav-item">
-              <a class="nav-link" href="/#" on:click={logout}>Log Out</a>
-            </li>
-          {:else}
-            <li class="nav-item">
-              <a class="nav-link" href="/#" on:click={login}>Log In</a>
-            </li>
-          {/if}
-        </ul>
-      </span>
-    </div>
-  </nav>
-
-  <!-- Application -->
-  {#if !$isAuthenticated}
-    <div class="container mt-5">
-      <div class="row">
-        <div class="col-md-10 offset-md-1">
-          <div class="jumbotron">
-            <h1 class="display-4">Task Management made Easy!</h1>
-            <p class="lead">Instructions</p>
-            <ul>
-              <li>Login to start &#128272;</li>
-              <li>Create Tasks &#128221;</li>
-              <li>Tick off completed tasks &#9989;</li>
-            </ul>
-            <a
-              class="btn btn-primary btn-lg mr-auto ml-auto"
-              href="/#"
-              role="button"
-              on:click={login}>Log In</a
-            >
-          </div>
-        </div>
-      </div>
-    </div>
-  {:else}
-    <div class="container" id="main-application">
-      <div class="row">
-        <div class="col-md-6">
-          <ul class="list-group">
-            {#each $user_tasks as item (item.id)}
-              <TaskItem task={item} />
-            {/each}
-          </ul>
-        </div>
-        <div class="col-md-6">
-          <input
-            class="form-control"
-            bind:value={newTask}
-            placeholder="Enter New Task"
-          />
-          <br />
-          <button type="button" class="btn btn-primary" on:click={addItem}>
-            Add Task
-          </button>
-        </div>
-      </div>
-    </div>
-  {/if}
 </main>
 
 <style lang="postcss">
