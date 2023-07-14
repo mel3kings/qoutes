4c4702b1a991a500eb9f5106ba055d6c8562a2cd
Melchor Tatlonghari <meltatlonghari3@gmail.com>
2023-07-14 13:35:26 +0700

    refactor: Change import of auth0 config variables

--- src/authService.js
+++ src/authService.js
@@ -1,11 +1,11 @@
 import { createAuth0Client } from "@auth0/auth0-spa-js";
 import { user, isAuthenticated, popupOpen } from "./store";
-import { config } from "../auth_config";
+import { DOMAIN, CLIENT_ID } from "../config";
 
 async function createClient() {
   let auth0Client = await createAuth0Client({
-    domain: config.domain,
-    clientId: config.clientId,
+    domain: DOMAIN,
+    clientId: CLIENT_ID,
   });
 
   return auth0Client;
